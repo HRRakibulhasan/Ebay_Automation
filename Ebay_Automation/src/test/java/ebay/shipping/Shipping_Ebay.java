0adeed96b2446bac9a707db04be4bbc58105d342
rakibulhasan <rakibulhasan@rakibuls-mbp>
2022-03-01 00:49:27 -0800

    Rakibul pushing new project.

---
+++ Ebay_Automation/src/test/java/ebay/shipping/Shipping_Ebay.java
@@ -0,0 +1,27 @@
+package ebay.shipping;
+
+public class Shipping_Ebay {
+ 
+	static int num;
+	static String name;
+	static double Doubledata ;
+ static boolean abcd=true;
+ static float bcd;
+ static long cbd;
+	
+	
+public static void main(String[] args) {
+	
+	
+	System.out.println(num);
+	
+System.out.println(name);
+
+System.out.println(Doubledata);
+
+System.out.println(abcd);
+System.out.println(bcd);
+System.out.println(cbd);
+
+}
+}
